30c0d06a29c14fd5fc33c70f14a45d33e65555d0
Ishita Pradhan <pradhanishitafree0@gmail.com>
2025-10-16 03:15:40 +0530

    Initial commit - React Practice Series setup

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Test from "./test"
+
+
+function App() {
+  const username = "Ishita"
+
+  return (
+    <>
+    <Test/>
+    <h1>Hello {username}</h1>
+  
+   </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App.jsx'
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Hello World </h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+// const anotherElement = (
+//     <a href="https://google.com" target='_blank'>Visit google</a>
+// )
+
+
+
+// const anotherUser = "Ishu"
+
+// const reactElement = React.createElement(
+//     'a',
+//     {href: 'https://google.com',target: '_blank' },
+//     'click me to visit google',
+//     anotherElement
+// )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+  
+
+
+
